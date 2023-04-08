708c460193ecaf7f9f0829483a46365c253219d4
CASAhmadou <amalsakho92@gmail.com>
2023-04-08 10:06:34 +0000

    initial project

---
+++ src/main/java/cas/java/scolarity/ScolarityApplication.java
@@ -0,0 +1,13 @@
+package cas.java.scolarity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScolarityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScolarityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cas/java/scolarity/ScolarityApplicationTests.java
@@ -0,0 +1,13 @@
+package cas.java.scolarity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScolarityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
